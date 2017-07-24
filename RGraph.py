3cbff89ab331106448364c3776226db8d05672c0
Valerie McCulloch <vmcculloch@smith.edu>
2017-07-23 20:20:42 -0400

    formatting and code struct

--- RGraph.py
+++ RGraph.py
@@ -16,62 +16,67 @@ import random
 
 
 class Node:
+
     def __init__(self, p, r, i):
-        self.p = p 
+        self.p = p
         self.x = p.getX()
         self.y = p.getY()
         self.r = r
-        self.id = i #id number 
-        self.circle = Circle(p,r)
+        self.id = i  # id number
+        self.circle = Circle(p, r)
+
     def draw(self, win):
         self.circle.setFill('black')
         self.circle.draw(win)
+
     def getCenter(self):
         return self.p
+
     def getID(self):
         return self.id
+
     def getX(self):
         return self.x
+
     def getY(self):
         return self.y
+
     def color(self, color):
         self.circle.setFill(color)
 
 
-
-
 class RGraph:
+
     def __init__(self, n, r, x, y):
-        self.n = n 
-        self.r = r 
+        self.n = n
+        self.r = r
         self.x = x
         self.y = y
-        self.d_rads = 6.28319/n # change in radians (2pi/n)
-        self.verts = [] # stores verts
-        self.c = 5 #small circle radius
+        self.d_rads = 6.28319 / n  # change in radians (2pi/n)
+        self.verts = []  # stores verts
+        self.c = 5  # small circle radius
 
     def draw(self, win):
         # init
-        R = self.r # radius
+        R = self.r  # radius
         l = self.c
-        rad = 0 # angle
+        rad = 0  # angle
         for i in range(self.n):
-            x = R*math.cos(rad) + self.x
-            y = R*math.sin(rad) + self.y
-            p = Point(x,y)
+            x = R * math.cos(rad) + self.x
+            y = R * math.sin(rad) + self.y
+            p = Point(x, y)
             c = Node(p, l, i)
-            self.verts.append(c) # store point
+            self.verts.append(c)  # store point
             c.draw(win)
-            rad+=self.d_rads # adjust angle
+            rad += self.d_rads  # adjust angle
 
     def getRand(self):
-        r1 = random.randint(0, self.n-1)
-        r2 = random.randint(0, self.n-1)
-        while r1==r2:
-            r2 = random.randint(0, self.n-1)
+        r1 = random.randint(0, self.n - 1)
+        r2 = random.randint(0, self.n - 1)
+        while r1 == r2:
+            r2 = random.randint(0, self.n - 1)
         return self.verts[r1], self.verts[r2]
-            
-            
+
     def isTouched(self, checkPoint):
         x = checkPoint.getX()
         y = checkPoint.getY()
@@ -79,15 +84,13 @@ class RGraph:
         for v in self.verts:
             v_x = v.getX()
             v_y = v.getY()
-            if x>v_x-c and x<v_x+c and y>v_y-c and y<v_y+c:
+            if x > v_x - c and x < v_x + c and y > v_y - c and y < v_y + c:
                 v.color('red')
-s                return True, v
+                return True, v
         return False, None
-            
-
-
-
-            
-            
-        
-
+    def isComplete(self, list1, list2):
+        N = self.n #number of verts
+        maxE = N*(N-1)/2
+        if len(list1)+len(list2)>=maxE:
+            return True
+        return False
--- ramsey.py
+++ ramsey.py
@@ -11,22 +11,25 @@ import math
 
 WIDTH = 500
 HEIGHT = 500
-N = 7
-rads = 6.28319 # 2 pi
+N = 3
+rads = 6.28319  # 2 pi
 R = 100
-P = [] #list of points
+P = []  # list of points
+count = 0
 
 # Edge lists
 User = []
 Comp = []
 
+
 def main():
     win = GraphWin('Ramsey', WIDTH, HEIGHT)
-    graph = RGraph(N, R, WIDTH/2, HEIGHT/2)
+    graph = RGraph(N, R, WIDTH / 2, HEIGHT / 2)
     graph.draw(win)
 
-    while True: #Game loop
-        while True: #user turn 
+    while True:  # Game loop
+        count = 0
+        while True:  # user turn
             clickPoint = win.getMouse()
             ans1, n1 = graph.isTouched(clickPoint)
             if ans1:
@@ -37,28 +40,33 @@ def main():
                         u = []
                         u.append(n1.getID())
                         u.append(n2.getID())
-                        User.append(u.sort())
+                        u.sort()
+                        User.append(u)
                         p1 = n1.getCenter()
                         p2 = n2.getCenter()
-                        line = Line(p1,p2)
+                        line = Line(p1, p2)
                         line.setFill('red')
                         line.draw(win)
                         break
                 break
-        while True: #Computer's turn
+        while True:  # Computer's turn
             n1, n2 = graph.getRand()
-            line = Line(n1.getCenter(),n2.getCenter())
-            line.setFill('blue')
-            line.draw(win)
-            
+            c = []
+            c.append(n1.getID())
+            c.append(n2.getID())
+            c.sort()
+            count+=1
+            if c not in Comp and c not in User:
+                Comp.append(c)
+                line = Line(n1.getCenter(), n2.getCenter())
+                line.setFill('blue')
+                line.draw(win)
+                break
+            elif count>=100:
+                break
+        if graph.isComplete(User, Comp):
+            print("done")
             break
-            
-            
-                    
-                    
-    
-
-
 
 
 main()
